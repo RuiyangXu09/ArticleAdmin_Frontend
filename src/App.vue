07228eb762edf28ffb8d759a9e284d4ee258b5e3
Ruiyang Xu <332971895@qq.com>
2024-04-07 15:03:16 +0800

    Correct the file name

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup>
-import Login from "@/views/Login.vue";
+import Register from "@/views/Register.vue";
 </script>
 
 <template>
-  <Login/>
+  <Register/>
 </template>
 
 <style scoped>
